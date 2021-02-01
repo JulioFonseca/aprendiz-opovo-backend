6f807dff6bdb31f5445e0d6dab782540558abc47
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 16:06:58 -0300

    criei um arquivo para armazenar o script do modal

---
+++ script.php
@@ -0,0 +1,10 @@
+
+	<script>
+
+	M.AutoInit();
+	document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+	</script>
