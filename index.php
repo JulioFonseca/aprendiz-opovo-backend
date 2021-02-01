0eebfe02b963c73ef1762c29a4a1e12ae1658cd8
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 10:13:37 -0300

    modifiquei o index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 	<title>homer</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php require_once 'conexao.php'; ?>
 </head>
 <body>
 	<h3>Clientes</h3>
+	<!--Tabela para READ Clientes-->
 	<table align="center">
 		<thead>
 			<tr>
@@ -23,9 +25,10 @@
 				<td>-----</td>
 				<td>-----</td>
 			</tr>
-		</tbody>
-		
+		</tbody>		
 	</table>
+	<br>
+	<a href="cadastro.php" class="btn">CADASTRAR</a>
 
 </body>
 </html>
