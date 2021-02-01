2893f9b22d29b70565265b183a06a8a450a39b04
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 11:12:35 -0300

    crie o sql para armazernar os arquivos no banco de dados

--- inserir.php
+++ inserir.php
@@ -1,3 +1,23 @@
 <?php
 	require_once 'conexao.php';
+	//recebendo os valores de cadastro.php e verificando se chegou na pagina apertando o button 
+	if (isset($_POST['btn-cadastrar'])) {
+		$nome = $_POST['nome'];
+		$email = $_POST['email'];
+		$cpf = $_POST['cpf'];
+		$senha = $_POST['senha'];
+		
+		//inserindo o cliente no banco de dados
+		$sql ="INSERT INTO clientes(nome, email, cpf, senha) VALUES('$nome','$email','$cpf','$senha')";
+
+		//verificando se houve um erro no cadastro
+		if (mysqli_query($conn, $sql)) {
+
+			      echo "Cadastro realizado com sucesso";
+		} else {
+
+		      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			
+		}
+	}
   ?>
