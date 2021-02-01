c6b926ecb4df10203797615f7499de0de7018d69
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 11:38:05 -0300

    criei o READ no arquivo index.php para consultar

--- index.php
+++ index.php
@@ -4,12 +4,18 @@
 	<title>homer</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php require_once 'conexao.php'; ?>
+	<?php 
+		require_once 'conexao.php'; 
+		$sql = "SELECT * FROM clientes";
+				$res = mysqli_query($conn, $sql); 
+
+				//if (mysqli_num_rows($res) > 0)
+	?>
 </head>
 <body>
 	<h3>Clientes</h3>
 	<!--Tabela para READ Clientes-->
-	<table align="center">
+	<table align="center" border="1">
 		<thead>
 			<tr>
 				<td>Nome</td>
@@ -18,6 +24,23 @@
 				<td>senha</td>
 			</tr>
 		</thead>
+				<?php 
+			if (mysqli_num_rows($res) > 0) {
+				while ($escrever = mysqli_fetch_array($res)) {
+				
+				?>
+				<tbody>
+			<tr>
+				<td><?php echo $escrever['nome'] ?></td>
+				<td><?php echo $escrever['email'] ?></td>
+				<td><?php echo $escrever['cpf'] ?></td>
+				<td><?php echo $escrever['senha'] ?></td>
+			</tr>
+		</tbody>
+		<?php 			
+						}
+			}else {
+		?>
 		<tbody>
 			<tr>
 				<td>-----</td>
@@ -25,7 +48,10 @@
 				<td>-----</td>
 				<td>-----</td>
 			</tr>
-		</tbody>		
+		</tbody>
+		<?php 
+			} 
+	?>		
 	</table>
 	<br>
 	<a href="cadastro.php" class="btn">CADASTRAR</a>
