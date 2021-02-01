b6b43e2b79749db60edf5268c399850c3d258170
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 09:49:46 -0300

    criei uma tabela no index.php

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+	<table align="center">
+		<thead>
+			<tr>
+				<td>Nome</td>
+				<td>email</td>
+				<td>cpf</td>
+				<td>senha</td>
+			</tr>
+		</thead>
+		
+	</table>
 
 </body>
 </html>
