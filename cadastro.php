39d1ac62bfb58deb0d718a10ebff7b36543a50f7
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 10:12:17 -0300

    criei o arquivo cadastro.php com o formulario

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>homer</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php require_once 'conexao.php'; ?>
+</head>
+<body>
+	<h3>Cadastrar</h3>
+		<form method="post" action="">
+			<input type="text" name="nome">
+			<input type="text" name="email">
+			<input type="text" name="cpf">
+			<input type="password" name="senha">
+			<button type="submit">cadastrar</button>
+		</form>
+	
+	<br>
+	<a href="" class="btn"></a>
+
+</body>
+</html>
