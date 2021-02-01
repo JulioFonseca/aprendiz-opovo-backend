d81f50c14b908bc48812c6805509a07769d11613
Julio Fonseca <juliocezar5716@gmail.com>
2021-02-01 19:11:49 -0300

    corrigir um erro na formataçao

--- editar.php
+++ editar.php
@@ -6,6 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 	<?php require_once 'conexao.php'; session_start(); ?>
 </head>
 <body>
@@ -47,7 +49,12 @@
                     <label for="email"> E-mail</label>
                 </div>
                 <div class="input-field col s12">
-                    <input type="text" name="cpf" id="cpf" class="form-control cpf-mask" maxlength="14" value="<?php echo $escrever['cpf'] ?>">
+                    <input type="text" name="cpf" id="cpf" class="form-control" maxlength="14" value="<?php echo $escrever['cpf'] ?>">
+                    <!-- mascara para o cpf -->
+                        <script type="text/javascript">
+                            $("#cpf").mask("000-000-000-00");
+                        </script>
+
                     <label for="cpf"> CPF</label>
                 </div>
                 <div class="input-field col s12">
@@ -55,6 +62,7 @@
                     <label for="senha"> Senha</label>
                 </div>
                 <button type="submit" name="btn-editar" class="btn">ATUALIZAR</button>
+                <a href="index.php" class="btn green">CLIENTES</a>
             </form>
         </div>
     </div>
